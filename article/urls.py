95dd2ce51602521af13afa1205f360302aaa9827
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-03-23 17:37:07 +0430

    create update delete detail is finised

--- article/urls.py
+++ article/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 
-app_name = 'article'
 urlpatterns = [
-    
-    path('', views.article, name='article')
+    path('', views.article_list_api_view, name='article'),
+    path('article_detail/<int:value_from_url>/', views.article_detail_api_view, name='article_detail'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,5 +1,52 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . models import Article
+from .Api.serializers import ArticleSerializer
 
-def article(request):
-    return HttpResponse('In the name of allah.')
+
+@api_view(["GET", "POST"])
+def article_list_api_view(reqeust):
+    if reqeust.method == "GET":
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif reqeust.method == "POST":
+        serializer = ArticleSerializer(data=reqeust.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def article_detail_api_view(reqeust, value_from_url):
+    try:
+        article = Article.objects.get(id=value_from_url)
+    except Article.DoesNotExist:
+        return Response(
+            {"error": {
+                "code": 404,
+                "message": f"there is no such table with the {value_from_url}."
+            }},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    if reqeust.method == "GET":
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    if reqeust.method == "PUT":
+        serializer = ArticleSerializer(instance=article, data=reqeust.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    if reqeust.method == "DELETE":
+        article.delete()
+        return Response({
+            "process": {
+                "code": 204,
+                "message": f"Article whose id was {value_from_url} has been deleted"
+            }}, status=status.HTTP_204_NO_CONTENT
+        )
